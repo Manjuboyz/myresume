98c75d3e6b019bc56ad516e87eff3bdefcf860b6
Manjuboyz <manjuboyz@gmail.com>
2020-02-09 16:57:49 +0530

    Updates

---
+++ precache-manifest.1433b8ef5959b4faf98924b17d9a250d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "814d9002dd6b7e19f9d6f8261a3a9819",
+    "url": "/MyResume/index.html"
+  },
+  {
+    "revision": "17e667280bd4985f5f28",
+    "url": "/MyResume/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "bbc94b3b1cdf736b01b4",
+    "url": "/MyResume/static/css/main.3977e833.chunk.css"
+  },
+  {
+    "revision": "17e667280bd4985f5f28",
+    "url": "/MyResume/static/js/2.87cc42ab.chunk.js"
+  },
+  {
+    "revision": "8956eb797ec1ca27a54b67665871977c",
+    "url": "/MyResume/static/js/2.87cc42ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbc94b3b1cdf736b01b4",
+    "url": "/MyResume/static/js/main.a8eb937a.chunk.js"
+  },
+  {
+    "revision": "bdd15342031d08fec504",
+    "url": "/MyResume/static/js/runtime-main.fc58f2dd.js"
+  },
+  {
+    "revision": "a56fdde9222be6b5b0eeea5fea6fc9d6",
+    "url": "/MyResume/static/media/manju.a56fdde9.jpg"
+  }
+]);
